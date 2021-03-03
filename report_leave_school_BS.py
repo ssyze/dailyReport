91c872c07eee3ccb8bf37d6ee78077f8e5c2e07c
Junyi Chen <46869158+ssyze@users.noreply.github.com>
2021-03-03 08:45:24 +0800

    Update report_leave_school_BS.py

--- report_leave_school_BS.py
+++ report_leave_school_BS.py
@@ -80,8 +80,8 @@ def daily_report(cookie,reportData,delayReport = False):  #最后个参数是补
         "p1$SuiSM": "绿色",
         "p1$LvMa14Days": "是",
         "p1$Address2": "",
-        "p1$ShiFZX":"否",
-        "p1$ShiFZJ": "是",
+        "p1$ShiFZX":"是",
+        "p1$ShiFZJ": "否",
         "F_TARGET": "p1_ctl00_btnSubmit",
         "p1_ContentPanel1_Collapsed": "true",
         "p1_GeLSM_Collapsed": "false",
@@ -91,6 +91,11 @@ def daily_report(cookie,reportData,delayReport = False):  #最后个参数是补
     header = {
         'X-FineUI-Ajax': 'true',
     }
+    if (reportData["location"] != "上海大学"):
+        data["p1$ShiFZX"]="否"
+        data["p1$ShiFZJ"]="是"
+        data["p1$ddlXian$Value"]="闵行区"
+        data["p1$ddlXian"]="闵行区"
     response = requests.post(reportUrl, data=data, cookies=cookie,headers=header)
     # print(response.text)
     return response.text.find("提交成功")
